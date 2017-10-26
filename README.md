aa8f847b02ba33a2c79631f136676ce0d05c5295
Felician Nemeth <nemethf@tmit.bme.hu>
2017-10-26 11:44:20 +0200

    Import initial version

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # bess-doc2wiki
 Automatically regenerate bess documentation and update the wiki page
+
+# Installation
+
+1. Clone `bess-doc2wiki`, `bess`, `bess.wiki`, and [Justine Sherry's
+`protoc-gen-doc`](https://github.com/justinemarie/protoc-gen-doc)
+repositories into a common directory.  `bess` and `bess.wiki` should
+not deviate from origin master, i.e., there should be no local
+development there.
+
+2. Compile protoc-gen-doc
+
+3. To automatically regenerate the wiki page after each commit to the
+   master you can
+
+(i) set up a web-hook in the github project page that runs this file,
+    or
+
+(ii) "watch" the project, and extend your .procmailrc file:
+```
+    :0
+    * ^List-Archive:.https://github.com/NetSys/bess
+    {
+        :0 cB: bess.lock
+        * ^Merged.#[0-9]
+        | $HOME/src/bess-doc2wiki/update.sh
+    }
+```
+
+It seems anyone can edit the wiki using github's the web interface,
+but `git push` is restricted to those how have write access to the
+project.  Therefore, my automatically updated version of the
+documentation is
+[here](https://github.com/nemethf/bess/wiki/Built-In-Modules-and-Ports)
---
+++ update.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+# Automatically regenerate bess documentation and update the wiki page
+# Copyright (C) 2017 Felicián Németh
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+BESS_DIR="$DIR"/../bess
+BESS_WIKI_DIR="$DIR"/../bess.wiki
+PROTOC_DIR="$DIR"/../protoc-gen-doc
+WIKI_PAGE=Built-In-Modules-and-Ports
+header_file=$(mktemp)
+quiet=-q
+
+cd "$BESS_WIKI_DIR"
+git reset $quiet --hard origin/master
+git pull $quiet origin master
+
+cd "$BESS_DIR"
+git pull $quiet origin master
+bess_rev=$(git describe --dirty --always --tags)
+
+
+PATH=$PATH:"$PROTOC_DIR"
+cd "$BESS_DIR"/protobuf
+
+cat >"$header_file" <<'EOF'
+__This text is auto-generated from comments in the module code.  To edit the contents of this page, please edit the comments in `bess/protobuf` in the main BESS repository.__  To see how this page is automatically re-generated after each commit, check out [this repository](https://github.com/nemethf/bess-doc2wiki).
+EOF
+
+protoc --doc_out=bess,Modules.md:. module_msg.proto
+cat "$header_file" Modules.md | sed 's/Command/\./g' | sed 's/Arg/()/g' > Modules.md.out
+mv Modules.md.out "$BESS_WIKI_DIR"/$WIKI_PAGE.md
+rm -f "$header_file"
+
+cd "$BESS_WIKI_DIR"
+if [[ $(git status --porcelain) ]]; then
+    git add $WIKI_PAGE.md
+    git commit -m "Regenerate $WIKI_PAGE for $bess_rev"
+    git push $quiet origin master
+fi
